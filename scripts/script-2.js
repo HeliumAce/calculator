e83e50285db5f734c0b1a6f51717854c8d2685f2
Liam Greig <helium.ace@gmail.com>
2020-05-15 12:31:54 -0400

    second attempt at multi num logic

--- scripts/script-2.js
+++ scripts/script-2.js
@@ -1,6 +1,9 @@
 const displayValue = document.getElementById('display');
 let currentNum = '';
-let currentCalc = [];
+let value1 = '';
+let value2 = '';
+let value3 = '';
+let currentOpp = '';
 
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
@@ -17,25 +20,53 @@ numbers.forEach(item => {
 
 operators.forEach(item => {
     item.addEventListener('click', event => {
-        console.log(item.innerHTML);
-        currentCalc.push(currentNum);
-        currentCalc.push(item.innerHTML);
+        setValues();
+        // checkValues();
+        if (currentOpp === '*' || currentOpp === '/') {
+            currentOpp = item.innerHTML;
+            console.log('currentOpp is ' + currentOpp);
+        } else if (currentOpp === '+' || currentOpp === '-') {
+            console.log('currentOpp is ' + currentOpp)
+        } else {
+            currentOpp = item.innerHTML;
+            console.log('currentOpp has been set to ' + currentOpp);
+        }
         currentNum = ''; 
         displayValue.innerHTML += item.innerHTML;
     })
 });
 
+function setValues() {
+    if (value1 != '' && value2 != '') {
+        value1 = operate(currentOpp, value1, value2);
+    } else if (value1 != '') {
+        value2 = currentNum;
+    } else {
+        value1 = currentNum;
+    }
+}
+
+/* function checkValues () {
+    if (currentOpp === '*' || currentOpp === '/') {
+        value1 = operate(currentOpp, value1, value2);
+    }
+} */
+
+
+
 clear.addEventListener('click', event => {
     displayValue.innerHTML =  '';
     currentNum = '';
-    currentCalc = [];
+    value1 = '';
+    value2 = '';
+    value3 = '';
+    currentOpp = '';
 });
 
 equals.addEventListener('click', event => {
-    currentCalc.push(currentNum);
-    displayValue.innerHTML =  operate(currentCalc[1], currentCalc[0], currentCalc[2]);
+    setValues();
+    displayValue.innerHTML =  operate(currentOpp, value1, value2);
     currentNum = displayValue.innerHTML;
-    currentCalc = [];
 });
 
 function operate (opp, a, b) {
