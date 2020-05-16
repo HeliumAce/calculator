9c1dd572421ffb51cbadbe1f006200977684c705
Liam Greig <helium.ace@gmail.com>
2020-05-16 14:30:58 -0400

    added current calc logic and display text

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ let value3 = '';
 let operator1 = '';
 let operator2 = '';
 let results = false;
+let currentCalc = [];
 
 const displayValue = document.getElementById('display');
 const latestCalc = document.getElementById('latestCalc');
@@ -13,15 +14,15 @@ const operators = document.querySelectorAll('.operator');
 const equals = document.getElementById('enter');
 const clear = document.getElementById('clear');
 
-latestCalc.innerHTML = 'I am a calculation';
-
 numbers.forEach(item => {
     item.addEventListener('click', event => {
         if (results == true) {
             value1 = '';
             displayValue.innerHTML =  '';
+            latestCalc.innerHTML = '';
             currentNum += item.innerHTML;
-            displayValue.innerHTML += item.innerHTML;
+            currentCalc = [];
+            displayValue.innerHTML = item.innerHTML;
             results = false;
         } else if (results == false) {
             currentNum += item.innerHTML;
@@ -69,13 +70,6 @@ function updateOperation () {
         value2 = operate(operator2, value2, value3);
         value3 = '';
         operator2 = '';
-   /* } else if (operator1 === '+' || operator1 === '-') {
-        if (operator2 === '+' || operator2 === '-') {
-            value1 = operate(operator1, value1, value2);
-            value2 = value3;
-            value3 = '';
-            operator1 = operator2;
-        } */
     } else if (operator2 === '+' || operator2 === '-') {
         value1 = operate(operator1, value1, value2);
         value2 = value3;
@@ -90,38 +84,40 @@ function pressEnter () {
         value2 = operate(operator2, value2, value3);
         value3 = '';
         operator2 = '';
-    /* } else if (operator1 === '+' || operator1 === '-') {
-        if (operator2 === '+' || operator2 === '-') {
-            value1 = operate(operator1, value1, value2);
-            value2 = value3;
-            value3 = '';
-            operator1 = operator2;
-        } */
     } else if (operator2 === '+' || operator2 === '-') {
         value1 = operate(operator1, value1, value2);
         value2 = value3;
         value3 = '';
         operator1 = operator2;
     }
+    currentCalc = [displayValue.innerHTML];
+    //currentCalc = currentCalc.toString().split('').join(' ');
+    //checkErrors();
 }
 
 clear.addEventListener('click', event => {
     displayValue.innerHTML =  '';
+    latestCalc.innerHTML = '';
     currentNum = '';
     value1 = '';
     value2 = '';
     operator1 = '';
     operator2 = '';
+    currentCalc = [];
 });
 
 equals.addEventListener('click', event => {
-    /* if (currentNum == '') {
+    if (currentNum == '' && value2 == '') {
+        displayValue.innerHTML = value1 + operator1;
+        return;
+    } else if (currentNum == '') {
         displayValue.innerHTML =  value1;
         return;
-    } */
+    }
     setValues();
     pressEnter();
     displayValue.innerHTML =  operate(operator1, value1, value2);
+    latestCalc.innerHTML = currentCalc + ' = ' + displayValue.innerHTML;
     currentNum = displayValue.innerHTML;
     value1 = currentNum;
     currentNum = '';
@@ -130,6 +126,26 @@ equals.addEventListener('click', event => {
     operator2 = '';
     results = true;
 });
+
+/* function checkErrors() {
+    if (!value1) {
+        results = true;
+        currentNum = '';
+        value1 = '';
+        value2 = '';
+        operator1 = '';
+        operator2 = '';
+        return displayValue.innerHTML = 'Calculation not supported';
+    } else if (!value2 && operator2) {
+        results = true;
+        currentNum = '';
+        value1 = '';
+        value2 = '';
+        operator1 = '';
+        operator2 = '';
+        return displayValue.innerHTML = 'Calculation not supported';
+    }
+}; */
 
 function operate (opp, a, b) {
     a = parseFloat(a);
